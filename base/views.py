0964734a73393497079640dc52a2ace1c05546a2
Arun7Raju <aruna@goschedule.io>
2024-09-28 11:59:49 +0530

    basic setup commit
    Signed-off-by: Arun7Raju <aruna@goschedule.io>

---
+++ base/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from django.views.generic import ListView
+from .models import Task
+
+class TaskList(ListView):
+    model = Task
